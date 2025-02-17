dafaf124009c6880af9d9dc3bc2d3880c5a6b25a
jgluiggi <jgluiggi@protonmail.com>
2025-02-17 16:52:22 -0300

    refactor: rotas mais descritivas

--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -3,10 +3,10 @@ import NoteController from "../controllers/noteController";
 
 const router = Router();
 
-router.get("/", NoteController.getAllNotes);
-router.get("/:id", NoteController.getNoteById);
-router.post("/", NoteController.createNote);
-router.put("/:id", NoteController.updateNote);
-router.delete("/:id", NoteController.deleteNote);
+router.get("/getAll", NoteController.getAllNotes);
+router.get("/getById/:id", NoteController.getNoteById);
+router.post("/create", NoteController.createNote);
+router.put("/update/:id", NoteController.updateNote);
+router.delete("/delete/:id", NoteController.deleteNote);
 
 export default router;
