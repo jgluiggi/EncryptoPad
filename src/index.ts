9786965821caa6e9d19f4d8053446944ca84a290
jgluiggi <jgluiggi@protonmail.com>
2025-02-17 17:27:32 -0300

    docs: setup e documentação de notas pelo swagger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
+import * as swaggerUi from "swagger-ui-express";
+import * as swaggerJSDoc from "swagger-jsdoc";
 import sequelize from "./config/database";
 import noteRoutes from "./routes/noteRoutes";
 
@@ -7,7 +9,22 @@ dotenv.config();
 
 const app = express();
 
+const swaggerOptions: swaggerJSDoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "EncryptoPad",
+            version: "0.1",
+            description: "API para o sistema EncryptoPad de anotações criptografadas.",
+            },
+    },
+    apis: ["./routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/notes", noteRoutes);
 
 const port = 3000;
--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -3,10 +3,150 @@ import NoteController from "../controllers/noteController";
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Note:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: number
+ *         title:
+ *           type: string
+ *         content:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ */
+
+/** 
+ * @swagger
+ * /getAll:
+ *   get:
+ *     summary: Retrieve all notes
+ *     description: Get a list of all notes
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Note'
+ */ 
 router.get("/getAll", NoteController.getAllNotes);
+
+/** 
+ * @swagger
+ * /getById/{id}:
+ *   get:
+ *     summary: Get a note by ID
+ *     description: Retrieve a specific note using its ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Note'
+ *       404:
+ *         description: Note not found
+ */ 
 router.get("/getById/:id", NoteController.getNoteById);
+
+/** 
+ * @swagger
+ * /create:
+ *   post:
+ *     summary: Create a new note
+ *     description: Add a new note to the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Note created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Note'
+ */
 router.post("/create", NoteController.createNote);
+
+ /** 
+ * @swagger
+ * /update/{id}:
+ *   put:
+ *     summary: Update a note
+ *     description: Modify an existing note
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Note updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Note'
+ *       404:
+ *         description: Note not found
+ */
 router.put("/update/:id", NoteController.updateNote);
+
+/** 
+ * @swagger
+ * /delete/{id}:
+ *   delete:
+ *     summary: Delete a note
+ *     description: Remove a note from the database
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Note deleted successfully
+ *       404:
+ *         description: Note not found
+ * 
+ */
 router.delete("/delete/:id", NoteController.deleteNote);
 
 export default router;
